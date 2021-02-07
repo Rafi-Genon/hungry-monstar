0bad0580f969007ca094dfb8ff434d383f1b70a8
Rafi-Genon <mdabulhasannurrafi@gmail.com>
2021-02-07 21:56:02 +0600

    assignment done

--- meal.js
+++ meal.js
@@ -1,20 +1,30 @@
+// global variable
 const mealsItem = document.getElementById("meals-item")
 const mealsInfo = document.getElementById("meals-info")
+
+// submit your meal
 document.getElementById("submit-btn").addEventListener('click', function () {
     const inputMeal = document.getElementById("input-meal").value
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputMeal}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
-            document.getElementById("search-result").innerHTML = `<h2 class="text-center text-success">You are lucky your '${inputMeal}' meal is ready</h2>`
             if (data.meals == null) {
-                document.getElementById("search-result").innerHTML = `<h2 class="text-center text-danger">Sorry '${inputMeal}' is not found</h2>`
+
+                // if your inputed meal doest match with api then it will say error
+                document.getElementById("search-result").innerHTML = `<h2 class="text-center bg-danger p-2 text-white">Sorry '${inputMeal}' meal is not found</h2>`
+                mealsInfo.innerHTML =''
+                mealsItem.innerHTML =''
             }
             else {
+                mealsInfo.innerHTML =''
+                document.getElementById("search-result").innerHTML = `<h2 class="text-center text-success">You are lucky your '${inputMeal}' meal is ready</h2>`
+                
+                // this will push available meal in api
+                // in 21 line I add an event handler
                 mealsItem.innerHTML = data.meals.map(meal => `
-                <div class="meal-thubnail-title shadow" onclick="titleClicked(${meal.idMeal})">
+                <div class="meal-thubnail-title shadow" onclick="displayMealDetails(${meal.idMeal})">
                     <img src="${meal.strMealThumb}"/>
-                    <h4 class="p-2">${meal.strMeal}</h4>
+                    <h5 class="p-2">${meal.strMeal}</h5>
                 </div>`
                 )
                     .join('');
@@ -24,7 +34,8 @@ document.getElementById("submit-btn").addEventListener('click', function () {
 
 })
 
-const titleClicked = id => {
+// if anyone clicked any meal item then this function will start
+const displayMealDetails = id => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
         .then(res => res.json())
         .then(data => renderMealDetails(data))
